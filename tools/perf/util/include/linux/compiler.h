6a26c450923dedb14941654d241ed31c70b76927
Khem Raj <raj.khem@gmail.com>
2012-01-05 11:42:35 -0800

    compiler.h: Undef before redefining __attribute_const__
    This is required to avoid warnings like
    util/include/linux/compiler.h:8:0: error: "__attribute_const__" redefined [-Werror]
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    
    Upstream-Status: Pending

--- tools/perf/util/include/linux/compiler.h
+++ tools/perf/util/include/linux/compiler.h
@@ -4,9 +4,11 @@
 #ifndef __always_inline
 #define __always_inline	inline
 #endif
+#undef __user
 #define __user
+#undef __attribute_const__
 #define __attribute_const__
-
+#undef __used
 #define __used		__attribute__((__unused__))
 
 #endif
